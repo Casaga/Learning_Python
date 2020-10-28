ebc797a3ee7d7284d642a0da2d6da9d587770825
Casaga <abraham253253@hotmail.es>
2020-10-27 18:11:26 -0600

    examples for IF/FOR/WHILE

---
+++ For.py
@@ -0,0 +1,25 @@
+"""Clase de For"""
+
+"""
+names = ('Abraham', 'Cesar','Daniel','Daniela', 'Diego', 'Edgar')
+
+for name in names:
+	print(f'Nombre del compita: {name}')
+else:
+	print('No more names')
+"""
+
+"""
+name= 'Mauricio'
+
+for char in name:
+	if char != 'i':
+		print(char)
+"""
+
+numbers=[]
+
+for number in range(0,21,2):
+	numbers.append(number)
+
+print(numbers)
